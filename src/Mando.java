d1c698ffd29bdd3b652328ff20a9f7effb4be32d
Harun <haruunseyma@gmail.com>
2022-05-07 20:40:57 +0300

    branc 2

--- src/Mando.java
+++ src/Mando.java
@@ -1,5 +1,6 @@
 public class Mando {
     public static void main(String[] args) {
         System.out.println("this is the branc example");
+        System.out.println("this is the branc 2");
     }
 }
